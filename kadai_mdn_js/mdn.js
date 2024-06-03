a214434d4de0fba17523e026072536284d8cc117
orihama-u9ri-aniani <165892486+orihama-u9ri-aniani@users.noreply.github.com>
2024-06-03 13:04:05 +0900

    kadai_mdn_jsを再ドロップ

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const a2223 = new Date("9, 22, 2023 00:00:00");
+
+const year = a2223.getFullYear();
+const month = a2223.getMonth();
+const day = a2223.getDate();
+
+const jpDate = (year + '年' + month + '月' + day + '日' );
+
+
+console.log(jpDate);
